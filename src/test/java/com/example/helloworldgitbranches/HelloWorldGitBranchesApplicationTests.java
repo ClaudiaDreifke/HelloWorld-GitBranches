9ed91a7304260611e4d0c2a7be4d8e5b415f996f
claudia <claudia.dreifke@gmail.com>
2022-07-13 15:55:24 +0200

    initial commit

---
+++ src/test/java/com/example/helloworldgitbranches/HelloWorldGitBranchesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.helloworldgitbranches;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldGitBranchesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
